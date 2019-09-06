ce415477eb83933c86333e54f2c2b9d980a191cd
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2019-09-05 17:35:06 -0700

    Update __init__.py

--- SparseTorch/__init__.py
+++ SparseTorch/__init__.py
@@ -6,10 +6,10 @@ from SpTo.CPUCupyPinned import GeneralOptimizerFactory
 from SpTo.CPUCupyPinned import SparseCOM
 from SpTo.CPUCupyPinned import DataGadget
 
-from SpTo.GPUSwitcherClasses import _GPUSparseCommon
-from SpTo.GPUSwitcherClasses import GPUSparseModelFactory
-from SpTo.GPUSwitcherClasses import GPUGeneralOptimizerFactory
-from SpTo.GPUSwitcherClasses import GPUSparseCOM
+from SpTo.GPUTorch import _GPUSparseCommon
+from SpTo.GPUTorch import GPUSparseModelFactory
+from SpTo.GPUTorch import GPUGeneralOptimizerFactory
+from SpTo.GPUTorch import GPUSparseCOM
 
 from SpTo.CPUTorchPinned import _CPUPytorchCommon
 from SpTo.CPUTorchPinned import CPUPytorchModelFactory
